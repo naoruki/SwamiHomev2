f5d69e520071074a7c24675ad359738f9ab4b6dd
Cornelia <naoruki@gmail.com>
2025-08-15 13:17:02 +0800

    some changes to nav bar and fix to residential services pages

--- src/components/NavBarMobile.jsx
+++ src/components/NavBarMobile.jsx
@@ -57,97 +57,173 @@ function NavBarMobile() {
             aria-labelledby="offcanvasNavbarLabel"
             placement="start"
           >
-<Offcanvas.Header closeButton>
-  <Link
-    to="/"
-    onClick={handleNavClick}
-    className="d-flex align-items-center text-decoration-none"
-  >
-    <img
-      src={Logo}
-      alt="Logo"
-      style={{ width: "70px", marginRight: "10px" }}
-    />
-    <span className="brand-text-small fw-bold text-wrap text-dark">
-      SUNSHINE WELFARE ACTION MISSION
-    </span>
-  </Link>
-</Offcanvas.Header>
+            <Offcanvas.Header closeButton>
+              <Link
+                to="/"
+                onClick={handleNavClick}
+                className="d-flex align-items-center text-decoration-none"
+              >
+                <img
+                  src={Logo}
+                  alt="Logo"
+                  style={{ width: "70px", marginRight: "10px" }}
+                />
+                <span className="brand-text-small fw-bold text-wrap text-dark">
+                  SUNSHINE WELFARE ACTION MISSION
+                </span>
+              </Link>
+            </Offcanvas.Header>
 
             <Offcanvas.Body>
               <Nav className="flex-column">
-                <Nav.Link as={Link} to="/" onClick={handleNavClick}>
+                {/* <Nav.Link as={Link} to="/" onClick={handleNavClick}>
                   Home
-                </Nav.Link>
+                </Nav.Link> */}
 
                 <NavDropdown title="About Us" id="about-dropdown">
-                  <NavDropdown.Item as={Link} to="/about-us" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/about-us"
+                    onClick={handleNavClick}
+                  >
                     About Us
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/about-us/#our-journey" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/about-us/#our-journey"
+                    onClick={handleNavClick}
+                  >
                     Our Journey
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/founder" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/founder"
+                    onClick={handleNavClick}
+                  >
                     Our Founder
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/organization" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/organization"
+                    onClick={handleNavClick}
+                  >
                     Organization
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/vision-mission" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/vision-mission"
+                    onClick={handleNavClick}
+                  >
                     Vision & Mission
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/annual-report" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/annual-report"
+                    onClick={handleNavClick}
+                  >
                     Annual Report
                   </NavDropdown.Item>
                 </NavDropdown>
 
                 <NavDropdown title="Services" id="services-dropdown">
-                  <NavDropdown.Item as={Link} to="/residential-services" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/residential-services"
+                    onClick={handleNavClick}
+                  >
                     Residential Services
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/day-care" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/day-care"
+                    onClick={handleNavClick}
+                  >
                     Day Care Services
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/home-based" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/home-based"
+                    onClick={handleNavClick}
+                  >
                     Home-Based Services
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/therapy" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/therapy"
+                    onClick={handleNavClick}
+                  >
                     Therapy Services
                   </NavDropdown.Item>
                 </NavDropdown>
 
                 <NavDropdown title="Community Life" id="community-dropdown">
-                  <NavDropdown.Item as={Link} to="/activities" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/activities"
+                    onClick={handleNavClick}
+                  >
                     Activities & Programs
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/facilities" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/facilities"
+                    onClick={handleNavClick}
+                  >
                     Facilities
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/visual-tour" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/visual-tour"
+                    onClick={handleNavClick}
+                  >
                     Visual Tour
                   </NavDropdown.Item>
                 </NavDropdown>
 
                 <NavDropdown title="Media" id="media-dropdown">
-                  <NavDropdown.Item as={Link} to="/news" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/news"
+                    onClick={handleNavClick}
+                  >
                     What's Happening
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/events" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/events"
+                    onClick={handleNavClick}
+                  >
                     Past Event Highlights
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/gallery" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/gallery"
+                    onClick={handleNavClick}
+                  >
                     Photo Gallery
                   </NavDropdown.Item>
                 </NavDropdown>
 
                 <NavDropdown title="Get Involved" id="get-involved-dropdown">
-                  <NavDropdown.Item as={Link} to="/careers" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/careers"
+                    onClick={handleNavClick}
+                  >
                     Careers
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/donate" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/donate"
+                    onClick={handleNavClick}
+                  >
                     Donate
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/volunteer" onClick={handleNavClick}>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/volunteer"
+                    onClick={handleNavClick}
+                  >
                     Volunteer
                   </NavDropdown.Item>
                 </NavDropdown>
--- src/pages/ResidentialServicesPage.jsx
+++ src/pages/ResidentialServicesPage.jsx
@@ -1,12 +1,29 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import ResidentailSerivcesHome from "../components/ResidentialServicesHero";
 import ResidentialSerivcesSection from "../components/ResidentialServicesSection";
-const ResidentialServicesPage = () => (
+const ResidentialServicesPage = () => {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      // Wait for DOM to render
+      setTimeout(() => {
+        const element = document.querySelector(hash);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 0);
+    }
+  }, [hash]);
+  return (
     <Container>
-        <ResidentailSerivcesHome />
-        <ResidentialSerivcesSection/>
-        {/* Add more components or content for the residential services page here */}
+      <ResidentailSerivcesHome />
+      <ResidentialSerivcesSection />
+      {/* Add more components or content for the residential services page here */}
     </Container>
-);
+  );
+};
 
 export default ResidentialServicesPage;
