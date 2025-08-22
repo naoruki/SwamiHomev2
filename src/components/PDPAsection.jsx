0977c8e6110005afb1d1c5b124dcb7e15d982f86
Cornelia <naoruki@gmail.com>
2025-08-22 15:09:53 +0800

    add PDPA

--- src/components/PDPAsection.jsx
+++ src/components/PDPAsection.jsx
@@ -1,7 +1,5 @@
-import { Container } from "react-bootstrap";
-
 const PdpaSection = () => (
-  <Container>
+  <>
     <div className="header-hero">
       <h1>Personal Data Protection Policy(PDPA)</h1>
     </div>
@@ -15,13 +13,78 @@ const PdpaSection = () => (
         protection while giving Organisations the confidence to harness personal
         data for innovation. It is our policy to comply with the requirements of
         PDPA Act 2012 and the guidelines issued by PDPC of Singapore from time
-        to time.{" "}
-      </p>	
+        to time.
+      </p>
       <p>
         <b>Collection of Personal Data</b>
       </p>
+      <p>
+        We collect personal data (name, IC no., residential address, mobile
+        number, email address, bank account number, medical records etc.) from
+        our Donors, Volunteers, Residents, Day Care clients, Home Care clients,
+        Employees, Contractors and Visitors for the following purposes:-
+      </p>
+      <ul>
+        <li>Events</li>
+        <li>Visitations</li>
+        <li>
+          To facilitate clients with their medical and transport appointments.
+        </li>
+        <li>Employment records</li>
+        <li>Information requested by MOM, MOH, AIC, IRAS, Hospitals etc.</li>
+        <li>Deliver and Administer services provided by SWAMI Home.</li>
+        <li>
+          Contact Tracing in the event of a disease outbreak in the Nursing
+          Home.
+        </li>
+      </ul>
+      <p>
+        <b>Exceptions</b>
+      </p>
+      <p>
+        We shall seek your consent before collecting and using your personal
+        data except in the following circumstances :-
+      </p>
+      <ul>
+        <li>
+          Necessary to respond to an emergency that threatens the life, health
+          or safety of the individual or another individual.
+        </li>
+        <li>Visitations</li>
+        <li>
+          To facilitate clients with their medical and transport appointments.
+        </li>
+        <li>Necessary for any investigation or proceedings</li>
+        <li>Publicly available data.</li>
+        <li>Requested by Government Authorities</li>
+        <li>
+          Contact Tracing in the event of a disease outbreak in the Nursing
+          Home.
+        </li>
+      </ul>
+      <p>
+        <b>Withdrawal of Consent</b>
+      </p>
+      <p>
+        Should a volunteer or donor do not wish for us to use or disclose their
+        personal data, he/she has to inform our Organisation.
+      </p>
+      <p>
+        <b> When consent is generally not required:-</b>
+      </p>
+      <ul>
+        <li>Photos taken for family gatherings or personal photo.</li>
+        <li>
+          When Event is open to the public, taking photos is considered publicly
+          available data.
+        </li>
+      </ul>
+      <p>
+        We have taken steps to ensure that appropriate levels of protection are
+        in place to maintain the security and integrity of your personal data.
+      </p>
     </div>
-  </Container>
+  </>
 );
 
 export default PdpaSection;
