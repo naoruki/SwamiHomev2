f3c03f9e8e47ffd840d7156e4b5aac30aac9fb95
Cornelia <naoruki@gmail.com>
2025-12-10 13:51:40 +0800

    make the navbar front bold

--- src/pages/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage.jsx
@@ -1,9 +1,9 @@
 import Activities from "../components/ActivitiesSection.jsx";
-import horse1 from "../assets/commcare.png";
+import comcard from "../assets/commcare.png";
 
 const activitiesData = [
   {
-    image: horse1,
+    image: comcard,
     title: "Celebrating Community Care 2025",
     description:
       "Celebrating the spirit of community care and support. Look out for this corner for updates.",
